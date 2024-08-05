6e5b2e8098fd7ff95e35d38f7e8ee88f045b457c
NachoEsc <ignacio.escarcha001@gmail.com>
2024-08-05 04:22:53 -0300

    segundo commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//codigo js
+
+// declaracion de variables
+var nombre;
+
+//asignacion de valores
+nombre="Ignacio"//string o cadena
+
+let apellidoPaterno;
+let apellidoMaterno = "Gonzalez"//inicializacion
+
+apellidoPaterno="Escarcha"
+
+let edad;
+
+edad=20;
+edad=21;//reasignando valores
+
+const PI=3.1416;
+//las constantes no se puede reasignar
+const GRAVEDAD=9.8;
+
+let nombreCompleto=nombre + " " + apellidoPaterno;//concatenacion
+
+let resultado= PI + GRAVEDAD + 5.4;
+
+//metodos de entrada y salida de datos
+
+console.log(edad)
