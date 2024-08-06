534a48646f6db992c2543db876b7d3a1887b4fe3
NachoEsc <ignacio.escarcha001@gmail.com>
2024-08-05 22:26:33 -0300

    cuarto commmit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-//variables
-let talle= prompt("Ingresa tu talle de remera")
+//variable condicional
+/*let talle= prompt("Ingresa tu talle de remera")
 
 if(talle=="xl"){
     console.log("Quedan 5 remeras")
@@ -23,4 +23,39 @@ else if (talle=="xxl") {
     console.log("No hacemos remeras de ese talle")
 }
 
-console.log("No hay remeras de su talle en stock")
+console.log("No hay remeras de su talle en stock") */
+
+
+/*1-paso: seria bienvenida
+2-paso: seria ingresar tu nombre
+3-paso: seleccionar remera
+4-paso: seleccionar talle
+5-paso: seleccionar color
+6-paso: Monto a pagar
+7-paso: compra finalizada
+*/
+
+//Primera pantalla
+alert("Bienvenido a Kurai Clothes")
+
+let talle= prompt("Ingresa tu nombre")
+
+let producto= "remeras";
+
+let ingresar= false;
+
+for (let i = 3; i >=1 ; i = i - 1) {
+    let ingreso = prompt("Ingresa el producto que deseas comprar, solo tenemos remeras");
+    if(producto === ingreso) {
+        alert("Seleccionaste remeras");
+        ingresar=true
+        break;
+    } else{
+        alert("Error ingresa el producto nuevamente");
+    }
+}
+
+//segunda pantalla
+if (ingresar === true) {
+    alert ("Ingrese su talle")
+}
